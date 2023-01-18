e8506f6d00ec9123f8a92cc7218873cf30108554
AlEzDuarte <alfre27ezequiel@gmail.com>
2023-01-17 21:33:24 -0300

    cambie la declaracion del objeto a librofav

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const lista = [nombre = "Alfredo Ezequiel Duarte", edad = 20, desarrollador = false, fecha_naci = new Date(2002,9,4), libro = {
+const lista = [nombre = "Alfredo Ezequiel Duarte", edad = 20, desarrollador = false, fecha_naci = new Date(2002,9,4), libroFav = {
     titulo: "Nunca te pares",
     autor: "Phil Knight",
     fecha: new Date(2016,3,26),
