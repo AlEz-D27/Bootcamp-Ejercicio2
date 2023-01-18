ed489d10d0cb61798e8730322bfb0b9efea7136e
AlEzDuarte <alfre27ezequiel@gmail.com>
2023-01-17 21:17:19 -0300

    segundo ejercicio de bootcamp

---
+++ index.js
@@ -0,0 +1,8 @@
+const lista = [nombre = "Alfredo Ezequiel Duarte", edad = 20, desarrollador = false, fecha_naci = new Date(2002,9,4), libro = {
+    titulo: "Nunca te pares",
+    autor: "Phil Knight",
+    fecha: new Date(2016,3,26),
+    URL: "https://superfilesxl.com/files-upload/9324ntppk.pdf"
+}];
+
+console.log(lista)
